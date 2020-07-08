1509615c4d797fc5f076ca49e2b94a77ad19c7a4
YuFu12138 <2529230361@qq.com>
2020-07-08 17:56:21 +0800

    simple backend

---
+++ project_backend/src/main/java/project_backend/org/utils/msgutils/MsgUtil.java
@@ -0,0 +1,26 @@
+package project_backend.org.utils.msgutils;
+import net.sf.json.JSONObject;
+public class MsgUtil {
+    public static final int SUCCESS = 0;
+    public static final int ERROR = -1;
+    public static final int LOGIN_USER_ERROR = -100;
+    public static final int NOT_LOGGED_IN_ERROR = -101;
+    public static final int REGISTER_ERROR=-200;
+
+    public static final String SUCCESS_MSG = "成功！";
+    public static final String LOGIN_SUCCESS_MSG = "登录成功！";
+    public static final String LOGOUT_SUCCESS_MSG = "登出成功！";
+    public static final String LOGOUT_ERR_MSG = "登出异常！";
+    public static final String ERROR_MSG = "错误！";
+    public static final String LOGIN_USER_ERROR_MSG = "用户名或密码错误，请重新输入！";
+    public static final String NOT_LOGGED_IN_ERROR_MSG = "登录失效，请重新登录！";
+    public static final String REGISTER_ERROR_MSG="用户名已被占用！";
+    public static final String REGISTER_SUCCESS_MSG="注册成功！";
+    public static Msg makeMsg(int status, String msg, JSONObject data){
+        return new Msg(status, msg, data);
+    }
+
+    public static Msg makeMsg(int status, String msg){
+        return new Msg(status, msg);
+    }
+}
