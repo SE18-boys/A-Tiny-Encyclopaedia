1de74e21a79c502867ccb4904a5df3847b235d2d
MikeShaw1 <42696208+MikeShaw1@users.noreply.github.com>
2020-07-22 12:16:25 +0800

    feat:add footer test

--- frontend/src/function/button.js
+++ frontend/src/function/button.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 class Button extends React.Component{
-  render(){
-    return (
-        <button>
-          {this.props.text}
-        </button>
-    )
-  }
+    render(){
+        return (
+            <button onClick={this.props.onClick}>
+                {this.props.text}
+            </button>
+        )
+    }
 }
 
 export default Button
