fbb2a2da637ba722d36a7e012332cec7fb984cc0
YuFu12138 <2529230361@qq.com>
2020-08-16 18:18:01 +0800

    doc:fix display error

--- frontend/src/components/HeaderInfo.js
+++ frontend/src/components/HeaderInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Input } from 'antd';
+import {Row, Col, Input, message} from 'antd';
 import '../css/index.css'
 import logo from "../assets/logo1.png";
 import {AdminAvatar} from "./AdminAvatar";
@@ -12,6 +12,18 @@ export class HeaderInfo extends React.Component {
         if (history.location.pathname !== "/")
         history.push("/")
     }
+    search = (value) => {
+        console.log(value);
+        if(value===''){
+            message.info("查询的词条不能为空！",1)
+        }
+        //let params={'name':value};
+        else {
+                history.push('/Details?search=' + value);
+            //console.log(params);
+            //searchDetails(params)
+        }
+    };
 
     render(){
         // const user = JSON.parse(localStorage.getItem("user"));
@@ -24,7 +36,9 @@ export class HeaderInfo extends React.Component {
                          <a>  <img alt="logo" onClick={this.backToHome} src={logo} style={{height: 60}} id="logo"/> </a>
                         </Col>
                         <Col span={6}>
-                            <Search/>
+                            <Search placeholder="请输入查询词条"
+                                    onSearch={(value) => this.search(value)}
+                            />
                         </Col>
                         <Col span={6} offset={9}>
                             <AdminAvatar/>
