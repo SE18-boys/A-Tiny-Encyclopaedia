8de071d4ccdc0fbdf6b9833ad30911f76a3e1f06
MikeShaw1 <42696208+MikeShaw1@users.noreply.github.com>
2020-09-07 20:50:20 +0800

    feat:add cache using ehcache.

--- project_backend/src/main/java/project_backend/org/ProjectApplication.java
+++ project_backend/src/main/java/project_backend/org/ProjectApplication.java
@@ -2,10 +2,12 @@ package project_backend.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
+@EnableCaching
 public class ProjectApplication {
 
     public static void main(String[] args) {
